7f108f905fa1aa3e6830e9235c2b7cd191991a45
anghu3 <935331858@qq.com>
2018-09-19 11:00:47 +0800

    add run_clgl

--- runner_HTMLreport/RUN_CLGL.py
+++ runner_HTMLreport/RUN_CLGL.py
@@ -10,14 +10,14 @@ import HTMLTestRunner
 import time
 
 def allcase():
-    case_dir_rk=r'F:\pythonkeys\自动化测试\lasa\RKGL'
+    case_dir_rk=r'F:\pythonkeys\自动化测试\lasa\CLGL'
     discover=unittest.defaultTestLoader.discover(case_dir_rk,pattern='test*.py',top_level_dir=None)
     return discover
 
 if __name__ == '__main__':
     now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
-    report_path = '../report/' + now + '拉萨边防自动化测试报告.html'
+    report_path = '../report/' + now + '拉萨边防-车辆管理自动化测试报告.html'
     fp = open(report_path, 'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='拉萨边防自动化测试报告', description='用例执行情况：')
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='拉萨边防-车辆管理自动化测试报告', description='用例执行情况：')
     runner.run(allcase())
     fp.close()
